d9ae4896366e91b2f8ca63f420ca09189ffe29c3
Riyaz Ahamed <riyaz.ahamed@travelyaari.com>
2017-04-18 13:32:14 +0530

    Reformat

--- app/src/main/java/com/mantis/ci/test/MainActivity.java
+++ app/src/main/java/com/mantis/ci/test/MainActivity.java
@@ -5,17 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    protected void Anbey1Aaruyireh() {
-        setContentView(R.layout.activity_main);
-    }
-
-    protected void Anbey2Aaruyireh() {
-        setContentView(R.layout.activity_main);
-    }
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
 }
