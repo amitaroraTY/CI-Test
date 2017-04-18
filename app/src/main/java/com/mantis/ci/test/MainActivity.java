c5d4e015b973c5e18367c8d61e296fe3138384a7
Riyaz Ahamed <riyaz.ahamed@travelyaari.com>
2017-04-18 15:51:51 +0530

    Add javadoc

--- app/src/main/java/com/mantis/ci/test/MainActivity.java
+++ app/src/main/java/com/mantis/ci/test/MainActivity.java
@@ -3,8 +3,8 @@ package com.mantis.ci.test;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/*
-* MainActivity
+/**
+ * MainActivity
  */
 public class MainActivity extends AppCompatActivity {
 
